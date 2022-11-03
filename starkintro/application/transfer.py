02e88d8c58386b9ead63cc7ae2982bc5f72bf016
Jonas Thomaz de Faria <jonasthomaz@gmail.com>
2022-11-03 11:41:07 -0300

    Create transfer Success

--- starkintro/application/transfer.py
+++ starkintro/application/transfer.py
@@ -1,7 +1,30 @@
 import starkbank
 from starkintro.infra.Config import Config
+from starkintro.domain.value_objects.transfer_notification import TransferNotification
 
 
 class Transfer:
 
-    def create_transfer(self, notification):
+    @property
+    def _stark_user(self):
+        return starkbank.Project(
+            environment=Config.STARK_ENVIRONMENT,
+            id=Config.STARK_ID,
+            private_key=Config.STARK_PRIVATE_KEY
+        )
+
+    def create_transfer(self, notification: TransferNotification):
+        transfers = starkbank.transfer.create([
+            starkbank.Transfer(
+                amount=notification.amount(),
+                tax_id=notification.tax_id(),
+                name=notification.name(),
+                bank_code=notification.bank_code(),
+                branch_code=notification.branch_code(),
+                account_number=notification.account_number(),
+                external_id=notification.external_id()
+            )
+        ], user=self._stark_user)
+
+        for transfer in transfers:
+            print(transfer)
--- starkintro/domain/value_objects/transfer_notification.py
+++ starkintro/domain/value_objects/transfer_notification.py
@@ -14,14 +14,42 @@ class TransferNotification(Notification):
 
     def __init__(self, args: dict):
         self.__amount = args.get("amount", 0)
-        self.__tax_id = args.get("amount", None)
-        self.__name = args.get("amount", None)
-        self.__bank_code = args.get("amount", None)
-        self.__branch_code = args.get("amount", None)
-        self.__account_number = args.get("amount", None)
-        self.__external_id = args.get("amount", None)
-        self.__scheduled = args.get("amount", None)
-        self.__tags = args.get("amount", [])
+        self.__tax_id = args.get("tax_id", None)
+        self.__name = args.get("name", None)
+        self.__bank_code = args.get("bank_code", None)
+        self.__branch_code = args.get("branch_code", None)
+        self.__account_number = args.get("account_number", None)
+        self.__external_id = args.get("external_id", None)
+        self.__scheduled = args.get("scheduled", None)
+        self.__tags = args.get("tag", [])
 
     def notification_validate(self):
-        ...
+        # Validacao dos campos
+        return True
+
+    def amount(self):
+        return self.__amount
+    def tax_id(self):
+        return self.__tax_id
+
+    def name(self):
+        return self.__name
+
+    def bank_code(self):
+        return self.__bank_code
+
+    def branch_code(self):
+        return self.__branch_code
+
+    def account_number(self):
+        return self.__account_number
+
+    def external_id(self):
+        return self.__external_id
+
+    def scheduled(self):
+        return self.__scheduled
+
+    def tags(self):
+        return self.__tags
+
